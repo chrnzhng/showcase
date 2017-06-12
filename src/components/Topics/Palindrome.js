f38edf89a9d914766873b2b3a9795249782f48fc
Jordan Zhang <chrn.zhng@gmail.com>
2017-06-12 17:39:40 -0600

    Finished all but one

---
+++ src/components/Topics/Palindrome.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+export default class Palindrome extends Component{
+
+
+    constructor() {
+        super();
+
+        this.state = {
+
+            userInput: [],
+            palindrome: ""
+        };
+    }
+
+     handleChange(val) {
+    this.setState({ userInput: val });
+  }
+
+    render() {
+        return ( 
+        <div className="puzzleBox filterStringPB">
+            <h4>Palindrome</h4>
+            <input className="inputLine" onChange={ (e) => this.handleChange(e.target.value) }></input>
+            <button className="confirmationButton" onClick={ () => this.isPalindrome(this.state.userInput) }> Check </button>
+            <span className="resultsBox"> Palindrome: { this.state.palindrome }</span>
+        </div>
+    
+        )
+    }
+}
